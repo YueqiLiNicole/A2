8016d945cfb678b84d557384d0c71ea30a6f1106
YueqiLiNicole <147017042+YueqiLiNicole@users.noreply.github.com>
2023-11-14 18:20:42 -0500

    Update search-photos.py

--- search-photos.py
+++ search-photos.py
@@ -5,7 +5,7 @@ from botocore.httpsession import URLLib3Session
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 from botocore.session import get_session
-from botocore.vendored import requests as botocore_requests  # Deprecated in newer version
+from botocore.vendored import requests as botocore_requests  # Deprecated in newer versions
 
 # Define the client to interact with Lex
 client = boto3.client('lexv2-runtime')
